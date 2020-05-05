552734f9c54f46ffb931b11645ae42314d30527b
oksanaHulyaeva <hulyaevaoksana@gmail.com>
2020-05-06 00:50:24 +0300

    fix: fix storage and styles

--- script.js
+++ script.js
@@ -1,70 +1,53 @@
 import dataObject from './js/dataObject.js';
 
-
+let isCapslock = false;
 const data = dataObject.dataObject;
 
 
-const isEnglish = localStorage.getItem('isEng') || 'true';
-let isCapslock = false;
+let isEnglish = localStorage.getItem('isEnglish') || true;
 
-if (localStorage.getItem('isEng') === null) {
-  localStorage.setItem('isEng', isEnglish);
+
+if (localStorage.getItem('isEnglish') === null) {
+  localStorage.setItem('isEnglish', isEnglish);
 }
 
-const capslockHandler = (elem) => {
-  const caps = elem.querySelector('.caps');
 
-  caps.addEventListener('click', () => {
-    if (!isCapslock) {
-      elem.innerHTML = '';
-      drawKeys(data.upperCaseEn, elem);
-      elem.querySelector('.caps').classList.add('active');
-      isCapslock = true;
-    } else {
-      elem.innerHTML = '';
-      drawKeys(data.lowerCaseEn, elem);
-      isCapslock = false;
-    }
-  });
+const classAdd = (elem, userClass) => {
+  elem.classList.add(userClass);
 };
-const keyStyleHandler = (elem) => {
-  elem.addEventListener('mousedown', (event) => {
-    const { target } = event;
-    if (target.tagName === 'SPAN' || target.tagName === 'SECTION' || target.innerText === 'Shift' || target.innerText === 'CapsLock') return;
-    target.classList.add('active');
-  });
-  elem.addEventListener('mouseup', (event) => {
-    if (event.target.className === 'keyboard-styles' || event.target.innerText === 'Shift' || event.target.innerText === 'CapsLock') return;
-    event.target.classList.remove('active');
-  });
+
+const classRemove = (elem, userClass) => {
+  elem.classList.remove(userClass);
 };
 
 const drawKeys = (arr, elem) => {
+  const board = elem;
+  board.innerHTML = '';
   for (let i = 0; i < arr.length; i += 1) {
     const newKey = document.createElement('div');
     newKey.id = data.codes[i];
 
     switch (arr[i]) {
-      case ('&#8592;'): newKey.classList.add('backspace');
+      case '&#8592;': newKey.classList.add('backspace');
         break;
-      case ('CapsLock'): newKey.classList.add('caps');
+      case 'CapsLock': newKey.classList.add('caps');
         break;
-      case ('Shift'):
+      case 'Shift':
         newKey.classList.add('shift');
         break;
-      case ('Tab'):
+      case 'Tab':
         newKey.classList.add('tab');
         break;
-      case ('Enter'):
+      case 'Enter':
         newKey.classList.add('enter');
         break;
-      case ('Ctrl'):
+      case 'Ctrl':
         newKey.classList.add('ctrl');
         break;
-      case ('Alt'):
+      case 'Alt':
         newKey.classList.add('alt');
         break;
-      case ('&emsp;'):
+      case '&emsp;':
         newKey.classList.add('space');
         break;
       default:
@@ -72,12 +55,128 @@ const drawKeys = (arr, elem) => {
     }
 
     newKey.innerHTML = arr[i];
-    elem.append(newKey);
+    board.append(newKey);
   }
+};
 
+const capslockHandler = (elem) => {
+  const board = elem;
+  const caps = elem.querySelector('.caps');
+
+  caps.addEventListener('click', () => {
+    board.innerHTML = '';
+    if (!isCapslock) {
+      checkRegister(board);
+      classAdd(board.querySelector('.caps'), 'active');
+    } else {
+      board.innerHTML = '';
+      checkRegister(board);
+    }
+  });
+};
+
+const shiftHandler = (elem) => {
+  const board = elem;
+  const shifts = document.querySelectorAll('.shift');
+
+  shifts.forEach((item) => {
+    item.addEventListener('mousedown', () => {
+      board.innerHTML = '';
+      checkRegister(board);
+      classAdd(document.querySelectorAll('.shift')[0], 'active');
+      classAdd(document.querySelectorAll('.shift')[1], 'active');
+    });
+
+    item.addEventListener('mouseup', () => {
+      board.innerHTML = '';
+      checkRegister(board);
+    });
+  });
+};
+
+const keyStyleHandler = (elem) => {
+  elem.addEventListener('mousedown', (event) => {
+    const { target } = event;
+    if (target.tagName === 'SPAN' || target.tagName === 'SECTION' || target.innerText === 'Shift' || target.innerText === 'CapsLock') return;
+    classAdd(target, 'active');
+    target.classList.add('active');
+  });
+  elem.addEventListener('mouseup', (event) => {
+    if (event.target.className === 'keyboard-styles' || event.target.innerText === 'Shift' || event.target.innerText === 'CapsLock') return;
+    classRemove(event.target, 'active');
+  });
+};
+
+const mainKeyHandler = (arr, elem) => {
+  drawKeys(arr, elem);
   capslockHandler(elem);
   shiftHandler(elem);
   keyStyleHandler(elem);
+};
+
+const checkRegister = (elem) => {
+  if (!isCapslock) {
+    if (!isEnglish) mainKeyHandler(data.upperCaseRu, elem);
+    else {
+      mainKeyHandler(data.upperCaseEn, elem);
+    }
+    classAdd(elem.querySelector('.caps'), 'active');
+    isCapslock = true;
+  } else {
+    if (!isEnglish) mainKeyHandler(data.lowerCaseRu, elem);
+    else {
+      mainKeyHandler(data.lowerCaseEn, elem);
+    }
+    isCapslock = false;
+  }
+};
+
+const clickSelectionHandler = (textfield, str) => {
+  const field = textfield;
+  const text = field.value;
+  const caretPos = field.selectionStart;
+  const caretPosEnd = field.selectionEnd;
+
+  field.value = text.substr(0, caretPos) + str + text.substr(caretPosEnd);
+  field.selectionStart = caretPos + str.length;
+  field.selectionEnd = caretPos + str.length;
+  field.focus();
+};
+
+const printOnClick = (keyboard, textfield) => {
+  const field = textfield;
+  keyboard.addEventListener('click', (event) => {
+    if (event.target.className === 'keyboard-styles') return;
+
+    const letter = event.target.innerText;
+    const position = field.selectionStart;
+
+    switch (event.target.innerText) {
+      case '←':
+        field.value = field.value.slice(0, -1);
+        break;
+      case 'Enter': field.value += '\n';
+        break;
+      case 'Tab': field.value += '\t';
+        break;
+      case '◄':
+        field.focus();
+        field.selectionStart = position - 1;
+        field.selectionEnd = position - 1;
+        break;
+      case '►':
+        field.focus();
+        field.selectionStart = position + 1;
+        field.selectionEnd = position + 1;
+        break;
+      case 'CapsLock':
+      case 'Ctrl':
+      case 'Alt':
+      case 'Win':
+      case 'Shift': break;
+      default: clickSelectionHandler(field, letter);
+    }
+  });
 };
 
 const drawKeyboard = () => {
@@ -95,171 +194,112 @@ const drawKeyboard = () => {
   container.append(textarea, keyboard, info);
   keyboard.classList.add('keyboard-styles');
 
-  if (localStorage.getItem('isEng') === 'true') {
-    drawKeys(data.lowerCaseEn, keyboard);
+  if (isEnglish) {
+    mainKeyHandler(data.lowerCaseEn, keyboard);
     textarea.focus();
     printOnClick(keyboard, textarea);
   }
 
-  if (localStorage.getItem('isEng') === 'false') {
-    drawKeys(data.lowerCaseRu, keyboard);
+  if (!isEnglish) {
+    mainKeyHandler(data.lowerCaseRu, keyboard);
     textarea.focus();
     printOnClick(keyboard, textarea);
   }
-};
-
-const printOnClick = (keyboard, textfield) => {
-  keyboard.addEventListener('click', (event) => {
-    if (event.target.className === 'keyboard-styles') return;
-
-    const letter = event.target.innerText;
-    const position = textfield.selectionStart;
-
-    switch (event.target.innerText) {
-      case '←':
-        textfield.value = textfield.value.slice(0, -1);
-        break;
-      case 'Enter': textfield.value += '\n';
-        break;
-      case 'Tab': textfield.value += '\t';
-        break;
-      case '◄':
-        textfield.focus();
-        textfield.selectionStart = position - 1;
-        textfield.selectionEnd = position - 1;
-        break;
-      case '►':
-        textfield.focus();
-        textfield.selectionStart = position + 1;
-        textfield.selectionEnd = position + 1;
-        break;
-      case 'CapsLock':
-      case 'Ctrl':
-      case 'Alt':
-      case 'Win':
-      case 'Shift': break;
-      default: clickSelectionHandler(textfield, letter);
-    }
-  });
-};
-
-const clickSelectionHandler = (textfield, str) => {
-  const text = textfield.value;
-  const caretPos = textfield.selectionStart;
-  const caretPosEnd = textfield.selectionEnd;
-
-  textfield.value = text.substr(0, caretPos) + str + text.substr(caretPosEnd);
-  textfield.selectionStart = caretPos + str.length;
-  textfield.selectionEnd = caretPos + str.length;
-  textfield.focus();
-};
-
-const shiftHandler = (elem) => {
-  const shift = document.querySelectorAll('.shift');
-
-  shift.forEach((item) => {
-    item.addEventListener('mousedown', () => {
-      elem.innerHTML = '';
-      if (!isCapslock) drawKeys(data.upperCaseEn, elem);
-      else drawKeys(data.lowerCaseEn, elem);
-      elem.querySelectorAll('.shift')[0].classList.add('active');
-      elem.querySelectorAll('.shift')[1].classList.add('active');
-    });
-    item.addEventListener('mouseup', () => {
-      elem.innerHTML = '';
-      if (!isCapslock) drawKeys(data.lowerCaseEn, elem);
-      else drawKeys(data.upperCaseEn, elem);
-    });
-  });
 };
 
 const printOnKeypress = () => {
   document.addEventListener('keydown', (event) => {
     event.preventDefault();
-    if (event.code === 'CapsLock') {
+
+    const eCode = event.code;
+    const targetKey = document.getElementById(eCode);
+
+    if (eCode === 'CapsLock') {
       event.preventDefault();
       if (isCapslock) {
         document.querySelector('.keyboard-styles').innerHTML = '';
-        drawKeys(data.lowerCaseEn, document.querySelector('.keyboard-styles'));
-        document.getElementById(event.code).classList.add('active');
+        checkRegister(document.querySelector('.keyboard-styles'));
+        classAdd(targetKey, 'active');
         isCapslock = false;
       } else {
         document.querySelector('.keyboard-styles').innerHTML = '';
-        drawKeys(data.upperCaseEn, document.querySelector('.keyboard-styles'));
+        checkRegister(document.querySelector('.keyboard-styles'));
         document.querySelectorAll('DIV').forEach((item) => {
-          item.classList.remove('active');
+          classRemove(item, 'active');
         });
         isCapslock = true;
       }
     }
 
-    if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
+    if (eCode === 'ShiftLeft' || eCode === 'ShiftRight') {
       event.preventDefault();
-      if (document.querySelector('.caps').classList.contains('active')) {
+
+      if (isCapslock) {
         document.querySelector('.keyboard-styles').innerHTML = '';
-        drawKeys(data.lowerCaseEn, document.querySelector('.keyboard-styles'));
-        document.querySelector('.caps').classList.add('active');
+        checkRegister(document.querySelector('.keyboard-styles'));
       } else {
         document.querySelector('.keyboard-styles').innerHTML = '';
-        drawKeys(data.upperCaseEn, document.querySelector('.keyboard-styles'));
+        checkRegister(document.querySelector('.keyboard-styles'));
       }
+      document.querySelectorAll('.shift').forEach((item) => {
+        classAdd(item, 'active');
+      });
     }
 
-    if (event.code === 'AltLeft' || event.code === 'AltRight' || event.code === 'Tab') {
+    if (eCode === 'AltLeft' || eCode === 'AltRight' || eCode === 'Tab') {
       event.preventDefault();
     }
 
-    switch (document.getElementById(event.code).innerText) {
+    const field = document.querySelector('TEXTAREA');
+    switch (targetKey.innerText) {
       case 'CapsLock':
       case 'Shift':
       case 'Ctrl':
       case 'Alt':
       case 'Win':
-        document.querySelector('TEXTAREA').value += '';
+        field.value += '';
         break;
       case 'Enter':
-        document.querySelector('TEXTAREA').value += '\n';
+        field.value += '\n';
         break;
       case 'Tab':
-        document.querySelector('TEXTAREA').value += '\t';
+        field.value += '\t';
         break;
       case '←':
-        document.querySelector('TEXTAREA').value = document.querySelector('TEXTAREA').value.slice(0, -1);
+        field.value = field.value.slice(0, -1);
         break;
       case '◄':
         event.preventDefault();
-        document.querySelector('TEXTAREA').focus();
-        document.querySelector('TEXTAREA').selectionStart = document.querySelector('TEXTAREA').selectionStart - 1;
-        document.querySelector('TEXTAREA').selectionEnd = document.querySelector('TEXTAREA').selectionEnd - 1;
+        field.focus();
+        field.selectionStart -= 1;
+        field.selectionEnd -= 1;
         break;
       case '►':
         event.preventDefault();
-        document.querySelector('TEXTAREA').focus();
-        document.querySelector('TEXTAREA').selectionStart = document.querySelector('TEXTAREA').selectionStart + 1;
-        document.querySelector('TEXTAREA').selectionEnd = document.querySelector('TEXTAREA').selectionEnd + 1;
+        field.focus();
+        field.selectionStart += 1;
+        field.selectionEnd += 1;
         break;
       default:
-        clickSelectionHandler(document.querySelector('TEXTAREA'), document.getElementById(event.code).innerText);
+        clickSelectionHandler(field, targetKey.innerText);
     }
-
-    document.getElementById(event.code).classList.add('active');
-    document.querySelector('TEXTAREA').focus();
+    classAdd(targetKey, 'active');
+    field.focus();
   });
 
   document.addEventListener('keyup', (event) => {
     document.querySelectorAll('DIV').forEach((item) => {
-      item.classList.remove('active');
+      classRemove(item, 'active');
       if (item.classList.contains('caps') && isCapslock) item.classList.add('active');
     });
 
     if (event.code === 'ShiftLeft' || event.code === 'ShiftRight') {
-      if (document.querySelector('.caps').classList.contains('active')) {
+      if (isCapslock) {
         document.querySelector('.keyboard-styles').innerHTML = '';
-        drawKeys(data.upperCaseEn, document.querySelector('.keyboard-styles'));
-        document.querySelector('.caps').classList.add('active');
+        checkRegister(document.querySelector('.keyboard-styles'));
       } else {
         document.querySelector('.keyboard-styles').innerHTML = '';
-        drawKeys(data.lowerCaseEn, document.querySelector('.keyboard-styles'));
+        checkRegister(document.querySelector('.keyboard-styles'));
       }
     }
     document.querySelector('TEXTAREA').focus();
@@ -270,31 +310,39 @@ const switchLanguage = () => {
   document.addEventListener('keydown', (event) => {
     if (!event.ctrlKey || !event.altKey) return;
     event.preventDefault();
+    const keyboard = document.querySelector('.keyboard-styles');
 
-    if (localStorage.getItem('isEng') === 'true' && document.querySelector('.caps').classList.contains('active')) {
-      localStorage.setItem('isEng', 'false');
-      document.querySelector('.keyboard-styles').innerHTML = '';
-      drawKeys(data.upperCaseRu, document.querySelector('.keyboard-styles'));
-    } else if (localStorage.getItem('isEng') === 'false' && document.querySelector('.caps').classList.contains('active')) {
-      localStorage.setItem('isEng', 'true');
-      document.querySelector('.keyboard-styles').innerHTML = '';
-      drawKeys(data.upperCaseEn, document.querySelector('.keyboard-styles'));
-    } else if (localStorage.getItem('isEng') === 'true' && !document.querySelector('.caps').classList.contains('active')) {
-      localStorage.setItem('isEng', 'false');
-      document.querySelector('.keyboard-styles').innerHTML = '';
-      drawKeys(data.lowerCaseRu, document.querySelector('.keyboard-styles'));
-    } else if (localStorage.getItem('isEng') === 'false' && !document.querySelector('.caps').classList.contains('active')) {
-      localStorage.setItem('isEng', 'true');
-      document.querySelector('.keyboard-styles').innerHTML = '';
-      drawKeys(data.lowerCaseEn, document.querySelector('.keyboard-styles'));
+    if (isEnglish && isCapslock) {
+      isEnglish = false;
+      localStorage.setItem('isEnglish', isEnglish);
+      keyboard.innerHTML = '';
+      mainKeyHandler(data.upperCaseRu, keyboard);
+    } else if (!isEnglish && isCapslock) {
+      isEnglish = true;
+      localStorage.setItem('isEnglish', isEnglish);
+      keyboard.innerHTML = '';
+      mainKeyHandler(data.upperCaseEn, keyboard);
+    } else if (isEnglish && !isCapslock) {
+      isEnglish = false;
+      localStorage.setItem('isEnglish', isEnglish);
+      keyboard.innerHTML = '';
+      mainKeyHandler(data.lowerCaseRu, keyboard);
+    } else if (!isEnglish && !isCapslock) {
+      isEnglish = true;
+      localStorage.setItem('isEnglish', isEnglish);
+      keyboard.innerHTML = '';
+      mainKeyHandler(data.lowerCaseEn, keyboard);
     } else {
-      localStorage.setItem('isEng', 'true');
+      isEnglish = true;
+      localStorage.setItem('isEnglish', isEnglish);
     }
   });
 };
 
-window.onload = function () {
+const keyboardHandler = () => {
   drawKeyboard();
   printOnKeypress();
   switchLanguage();
 };
+
+window.onload = keyboardHandler();
